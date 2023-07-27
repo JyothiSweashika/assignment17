3a55b3bf699a1df2b2c4b770bdd1764dd58018d7
JyothiSweashika <jyothisweashikaraavi@gmail.com>
2023-07-27 15:42:24 +0530

    assignment17

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(3);
+            circle.PrintInfo();
+
+            Rectangle rectangle = new Rectangle(6, 4);
+            rectangle.PrintInfo();
+
+            Triangle triangle = new Triangle(2, 4, 7);
+            triangle.PrintInfo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment17
+{
+    public class Shape
+    {
+        public string ShapeType { get; set; }
+
+        public virtual double Area { get; }
+        public virtual double Perimeter { get; }
+    }
+}
